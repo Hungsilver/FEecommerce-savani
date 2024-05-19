acc22ebfc3608bde71cd99a739c04f4f635dca72
hungsilver <hungsilvertq@gmail.com>
2024-05-19 14:24:11 +0700

    fix-api

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://beecommerce-savani.onrender.com/api/",
   timeout: 10000,
 });
 
